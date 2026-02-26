f6f309d7b43c2fcf18e670ff902d778f192ce44b
Tuğçe POTUK <160920337+Ttugce0@users.noreply.github.com>
2026-02-26 15:25:24 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,5 @@
 const admin = require("firebase-admin");
-
-if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT env değişkeni yok");
-}
-
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf-8")
-);
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
